cea3d01b077a85e69e5740a3e17df15fe802a57b
inocentjap-eng <inocentjap@gmail.com>
2026-08-18 13:55:18 +0300

    laksy 2.3

---
+++ teht_2.3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna kanta: "))
+korkeus = float(input("Anna korkeus: "))
+
+piiri = 2 * kanta + 2 * korkeus
+pinta_ala = kanta * korkeus
+
+print(f"Piiri on {piiru}")
+print(f"Pinta on {pinta_ala}")
