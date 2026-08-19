7c7f1fe484973acce96117fe72fea8cda1185ac6
inocentjap-eng <inocentjap@gmail.com>
2026-08-19 09:23:09 +0300

    teht 2.4

---
+++ teht_2.4.py
@@ -0,0 +1,12 @@
+num1 = int(input("Anna num1: "))
+num2 = int(input("Anna num2: "))
+num3 = int(input("Anna num3: "))
+
+summa = num1 + num2 + num3
+tulon = num1 * num2 * num3
+keski_arvo = summa / 3
+
+
+print(f"Lukujen summa on {summa}")
+print(f"Lukujen tulo on {tulon}")
+print(f"Lukujen keskiarvo on {keski_arvo}")
