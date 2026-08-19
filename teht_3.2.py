e48e8e44a1683cd4ee155f214fa2ce9636e1acac
inocentjap-eng <inocentjap@gmail.com>
2026-08-19 13:19:31 +0300

    teht 3.2

---
+++ teht_3.2.py
@@ -0,0 +1,17 @@
+hyttiluoka = input ("Anna laiva hyttiluoka: ")
+
+if hyttiluoka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+
+
+elif hyttiluoka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+
+elif hyttiluoka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+
+elif hyttiluoka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka")
