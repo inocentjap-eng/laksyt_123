572cf0f8f75158df7a4dda68579b7c643686eec7
inocentjap-eng <inocentjap@gmail.com>
2026-08-19 15:19:19 +0300

    2.2 update

--- teht_2.2.py
+++ teht_2.2.py
@@ -1,10 +1,9 @@
-from re import Match
 import math
 
 sade = float(input("Anna ympyran sade: "))
 
 pinta_ala = math.pi * sade**2
 
-print(f"pinta-ala on {pinta_ala}")
+print(f"pinta-ala on {pinta_ala:.2f}")
 
 
