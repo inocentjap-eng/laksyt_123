ef5bdde34287f453bda2b2f8613a2e3723de4250
inocentjap-eng <inocentjap@gmail.com>
2026-09-03 12:20:57 +0300

    Module_6

---
+++ Module_6/teht_6.4.py
@@ -0,0 +1,11 @@
+def laske_summa(luvut):
+    summa = sum(luvut)
+    return summa
+
+def laske_summa(luvut):
+    summa = sum(luvut)
+    return summa
+
+luvut = [3, 7, 12, 5, 9]
+summa = laske_summa(luvut)
+print(f"Listan lukujen summa on {summa}.")
