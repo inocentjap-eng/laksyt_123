aa70a8279292fbc6c1f2bbdbf603d1b2aa60a21c
inocentjap-eng <inocentjap@gmail.com>
2026-09-10 14:19:19 +0300

    Module_8

---
+++ Module_8/teht_8.1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='1234',
+         autocommit=True,
+         use_pure=True
+         )
+
+maakoodi = input("Anna maakoodi: ")
+
+sql = f"SELECT type, COUNT(*) FROM airport WHERE iso_country = '{maakoodi}' GROUP BY type"
+
+print(sql)
+kursori = yhteys.cursor()
+kursori.execute(sql)
+
+tulos = kursori.fetchall()
+
+for rivi in tulos:
+    print(rivi[0], ":", rivi[1], "airports")
